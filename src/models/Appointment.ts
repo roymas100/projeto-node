389f5f2df3e4477c8ce462e913ea0d7e0b34153b
roymas100 <amir.dantas@hotmail.com>
2020-12-22 15:01:24 -0300

    @ADD : Config and example of architecture

---
+++ src/models/Appointment.ts
@@ -0,0 +1,22 @@
+import { v4 as uuid } from 'uuid'
+
+interface AppointmentConstructor {
+  provider: string;
+  date: Date;
+}
+
+class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  constructor({ provider, date }: Omit<Appointment, 'id'>) {
+    this.id = uuid();
+    this.provider = provider;
+    this.date = date;
+  }
+}
+
+export default Appointment;
